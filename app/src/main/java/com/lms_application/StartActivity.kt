5b4b6cd4dc3a1ea32bb10cdd5943236080c0addd
Uthoai Marma <uthoaimarma597@gmail.com>
2025-11-25 16:22:58 +0600

    add start activity

---
+++ app/src/main/java/com/lms_application/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.lms_application
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_start)
+    }
+}
